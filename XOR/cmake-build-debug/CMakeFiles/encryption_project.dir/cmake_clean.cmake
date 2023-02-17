281a9376772a978bd51d0a9556ac74237fd2d4d0
Tu <92423514+tuvo0515@users.noreply.github.com>
2023-02-16 23:59:59 -0800

    Tu - XOR methods
    I just push everything in the folder

---
+++ XOR/cmake-build-debug/CMakeFiles/encryption_project.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/encryption_project.dir/main.cpp.o"
+  "encryption_project"
+  "encryption_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/encryption_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
